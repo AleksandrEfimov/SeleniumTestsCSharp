5b9f4919f1b61d91df481debf62e90510bd1d59c
Aleks.Efy <aleks.efy@gmail.com>
2017-09-03 01:15:13 +0300

    Вход в админ-панель litecart

--- CSharpExample/UnitTest1.cs
+++ CSharpExample/UnitTest1.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Opera;
 using OpenQA.Selenium.Support.UI;
 
 namespace csharp_example
@@ -16,23 +17,33 @@ namespace csharp_example
         public void Start()
         {
             driver = new ChromeDriver();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            
+            //driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(20));
+            
+            
+           wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
         [Test]
         public void FirstTest()
         {
-            driver.Url = "http://www.google.com/";
-            IWebElement element = wait.Until(d => d.FindElement(By.Name("q")));
-            driver.FindElement(By.Name("q")).SendKeys("webdriver");
-            //driver.FindElement(By.Name("btnK")).Click();
-            driver.FindElement(By.Name("btnK")).Submit();
-            wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+            driver.Url = "http://localhost:8080/litecart/admin/";
+            IWebElement element = wait.Until(d => d.FindElement(By.Name("username")));
+            
+
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            // в поисках ошибки применён xPath - не помог, но работает.
+            driver.FindElement(By.XPath("//*[@id=\"box-login\"]/form/div[1]/table/tbody/tr[2]/td[2]/span/input")).SendKeys("admin");
+            // данному логину необходим был Клик, в то время как Гугл требовал сабмит
+            driver.FindElement(By.Name("login")).Click();
+            // как критерий входа в админ-панель - виджет статистики интернет-магазина
+            wait.Until(ExpectedConditions.ElementExists(By.Id("widget-stats")));
         }
 
         [TearDown]
         public void Stop()
         {
+            //driver.FindElement(By.ClassName("fa fa-sign-out fa-lg")).Click();
             driver.Quit();
             driver = null;
         }
